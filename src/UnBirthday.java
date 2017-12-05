183dc782a92a5184b755e2df0e21ac3c65412b80
TJHL <league@wts15.attlocal.net>
2017-12-04 19:58:48 -0800

    UnBirthday, TheRiddler

---
+++ src/UnBirthday.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class UnBirthday {
+	public static void main(String[] args) {
+	String date=JOptionPane.showInputDialog("What's your Birthday?");
+	if(date.equals("12/4")) {
+		JOptionPane.showMessageDialog(null,"Happy Birthday!");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "Happy Un-Birthday");
+	}
+	}
+
+}
