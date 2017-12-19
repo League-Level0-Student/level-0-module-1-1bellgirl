9fbe94de75c262f99791d070d0c77a8703243c5b
TJHL <league@wts15.attlocal.net>
2017-12-18 19:57:24 -0800

    MyAges RobotInSpace

---
+++ src/MyAges.java
@@ -0,0 +1,16 @@
+
+public class MyAges {
+	public static void main(String[] args) {
+		for (int i = 2004; i <= 2017; i++) {
+System.out.println("In "+i+ " I was "+(i-2004)+ " years old.");
+if(i-2004==1) {
+	System.out.println("In "+i+ " I was "+(i-2004)+ " year old");
+}
+else if(i==2017) {
+	System.out.println("In "+i+ " I am "+(i-2004)+ " years old");
+		}
+
+
+}
+	}
+	}
